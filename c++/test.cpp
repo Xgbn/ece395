55a2545d80185c145d45fed41f161258b2ac43e7
HeyC4 <chenhuang444@gmail.com>
2016-03-30 23:22:14 -0500

    made serial intoto a class for easy use

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,41 +1,41 @@
 #include <string>
 #include <iostream>
-#include <boost/asio.hpp> 
-#include <boost/asio/serial_port.hpp> 
+#include "serial.h"
 
 
-using namespace boost;
+using namespace std;
 
 int main() {
-	asio::io_service io;
-	asio::serial_port port(io);
+	// asio::io_service io;
+	// asio::serial_port* port;
 
-	port.open("/dev/ttyUSB0");
-	port.set_option(asio::serial_port_base::baud_rate(115200));
+	// port = new asio::serial_port(io);
 
-	char c;
-	std::string line;
+	// port->open("/dev/ttyUSB0");
+	// port->set_option(asio::serial_port_base::baud_rate(115200));
 
-	// Read 1 character into c, this will block
-	// forever if no character arrives.
+	// char c;
+	// std::string line;
 
+	// while(1){
+	// 	line.clear();
+	// 	c = '1';
+	// 	asio::write(*port, asio::buffer(&c,1));
+	// 	while(1){
+	// 		asio::read(*port, asio::buffer(&c,1));
+	// 		if(c == '\n')
+	// 			break;
+	// 		line += c;
+	// 	}
+	// 	std::cout << line << std::endl << std::endl;
+	// }
+	// port->close();
+	// delete port;
+
+	serial_port port("/dev/ttyUSB0");
 	while(1){
-		line.clear();
-		c = '1';
-		asio::write(port, asio::buffer(&c,1));
-		while(1){
-			asio::read(port, asio::buffer(&c,1));
-			if(c == '\n')
-				break;
-			line += c;
-		}
-		std::cout << line << std::endl << std::endl;
+		cout << port.readline() << endl << endl;
 	}
-
-
-	asio::read(port, asio::buffer(&c,1));
-
-	port.close();
 
 	return 0;
 }
